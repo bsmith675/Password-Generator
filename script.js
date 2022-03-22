112c9852c8f653dac410208add5c77b65ba0dc70
Ben Smith <benlsmith329@gmail.com>
2022-03-22 18:37:24 -0400

    added console log on lines 61 and 66. Also added for function and return function on lines 63 and 68

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 //Array Characters
-var spcial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '|', '~', '`', '/', '?', ':'];
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '|', '~', '`', '/', '?', ':'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //function
 
-function question() {
+function questions() {
     var isValid = false;
     do {
-        var askNumbers = confirm("")
-        var length = prompt("")
-        var askUpperCase = confirm("");
-        var askLowerCase = confirm("");
-        var askSpecial = confirm("");
+        var askNumbers = confirm("Do you want your password to include numbers?");
+        var length = prompt("Choose a number between 8 and 128 characters to indicate password length");
+        var askUpperCase = confirm("Do you want your password to include upper case letters?");
+        var askLowerCase = confirm("Do you want your password to include lower case letters?");
+        var askSpecial = confirm("Do you want your password to include special characters?");
         var responses = {
             askSpecial: askSpecial,
             askUpperCase:  askUpperCase,
@@ -25,22 +25,23 @@ function question() {
             length: length
         }
         if((length < 8)||(length > 128))
-        alert ("Choose number between 8 and 128");
-        else if((!askNumbers)&&(!askLowerCase)&&(askUpperCase)&&(!askSpecial))
+        alert("Choose number between 8 and 128");
+        else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
         alert("Must choose at least one type.");
         else
-        isValid =true
+        isValid = true;
+
     } while(!isValid);
     return responses;
 }
 
 function generatePassword() {
     var possibleCombo = [];
-    var passwordOptions = question();
+    var passwordOptions = questions();
     var finalPassword = "";
 
     if (passwordOptions.askLowerCase) {
-        for (var i of upperCase)
+        for (var i of lowerCase)
         possibleCombo.push(i);
     }
     if (passwordOptions.askNumbers) {
@@ -57,6 +58,14 @@ function generatePassword() {
         possibleCombo.push(i);
     }
 
+    console.log(possibleCombo);
+
+    for (var i = 0; i < passwordOptions.length; i++) {
+        finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+    }
+    console.log(finalPassword);
+
+    return finalPassword;
 
 }
 
